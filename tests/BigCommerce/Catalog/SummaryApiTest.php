deec52e0b47afff98c24e489238c7f4a711fd6f4
Jarrod Swift <jarrod.swift@aligent.com.au>
2020-07-24 09:46:40 +0930

    Add test for Summary API

---
+++ tests/BigCommerce/Catalog/SummaryApiTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace BigCommerce\Tests\Catalog;
+
+
+use BigCommerce\Tests\BigCommerceApiTest;
+
+class SummaryApiTest extends BigCommerceApiTest
+{
+    public function testCanGetSummary(): void
+    {
+        $this->setReturnData('catalog__summary__get.json');
+
+        $this->assertEquals(
+            'Shop All',
+            $this->getApi()->catalog()->summary()->get()->getSummary()->primary_category_name
+        );
+    }
+}
