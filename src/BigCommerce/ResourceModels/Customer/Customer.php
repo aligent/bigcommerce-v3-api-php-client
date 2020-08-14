68e73f7ffb02e38ffa31ccc154e0a8170d903e8b
Matthew Smith <matthew.smith@aligent.com.au>
2020-08-14 13:47:44 +0930

    Add Customer Resource model

---
+++ src/BigCommerce/ResourceModels/Customer/Customer.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace BigCommerce\ApiV3\ResourceModels\Customer;
+
+use BigCommerce\ApiV3\ResourceModels\ResourceModel;
+
+/**
+ * @package BigCommerce\ApiV3\ResourceModels
+ */
+class Customer extends ResourceModel
+{
+    /**
+     * Typically need either an ID or an email in order to do anything useful with an existing customer
+     */
+    public int $id;
+    public string $email;
+    public string $first_name;
+    public string $last_name;
+    public string $company;
+    public string $street_1;
+    public string $street_2;
+    public string $city;
+    public string $state;
+    public string $zip;
+    public string $country;
+    public string $country_iso2;
+    public string $phone;
+
+}
