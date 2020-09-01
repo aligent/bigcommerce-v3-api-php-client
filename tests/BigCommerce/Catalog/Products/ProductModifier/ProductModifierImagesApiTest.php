c40b8331625cf72530556a4f9488da4df295414c
Jarrod Swift <jarrod.swift@aligent.com.au>
2020-09-01 11:26:09 +0930

    Add test stubs for new implementations

---
+++ tests/BigCommerce/Catalog/Products/ProductModifier/ProductModifierImagesApiTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace BigCommerce\ApiV3\Catalog\Products\ProductModifier;
+
+
+use BigCommerce\Tests\BigCommerceApiTest;
+
+class ProductModifierImagesApiTest extends BigCommerceApiTest
+{
+    public function testCanCreateImage()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanDeleteImage()
+    {
+        $this->markTestIncomplete();
+    }
+}
