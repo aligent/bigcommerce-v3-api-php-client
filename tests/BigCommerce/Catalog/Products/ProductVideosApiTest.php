f9e920857308eea695e049f47ccb5976e37ff7df
Jarrod Swift <jarrod.swift@aligent.com.au>
2020-09-01 11:50:45 +0930

    Add test stubs for ProductVideos API

---
+++ tests/BigCommerce/Catalog/Products/ProductVideosApiTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace BigCommerce\Tests\Catalog\Products;
+
+
+use BigCommerce\Tests\BigCommerceApiTest;
+
+class ProductVideosApiTest extends BigCommerceApiTest
+{
+    public function testHasProductAndVideoId(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanGetVideo(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanGetAllVideos(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
