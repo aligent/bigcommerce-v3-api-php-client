7e4157e9e8ebb2023e363c17d078fd99bfdf8e9b
Jarrod Swift <jarrod.swift@aligent.com.au>
2020-09-01 12:55:17 +0930

    Add test stub for Product Variant Metafields API

---
+++ tests/BigCommerce/Catalog/Products/ProductVariant/ProductVariantMetafieldsApiTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BigCommerce\ApiV3\Catalog\Products\ProductVariant;
+
+use BigCommerce\Tests\BigCommerceApiTest;
+
+
+class ProductVariantMetafieldsApiTest extends BigCommerceApiTest
+{
+    public function testHasProductVariantAndMetafieldIds()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanGetProductVariantMetafield()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanGetAllProductVariantMetafields()
+    {
+        $this->markTestIncomplete();
+    }
+}
