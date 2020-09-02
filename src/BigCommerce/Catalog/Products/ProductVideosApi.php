7f0fffaad8dd34ee57fdb98d53f7886814e94e87
Jarrod Swift <jarrod.swift@aligent.com.au>
2020-09-02 09:47:08 +0930

    Implement tests and make fix for Product Videos API

--- src/BigCommerce/Catalog/Products/ProductVideosApi.php
+++ src/BigCommerce/Catalog/Products/ProductVideosApi.php
@@ -5,16 +5,14 @@ namespace BigCommerce\ApiV3\Catalog\Products;
 
 
 use BigCommerce\ApiV3\Api\ResourceApi;
-use BigCommerce\ApiV3\ResponseModels\PaginatedResponse;
 use BigCommerce\ApiV3\ResponseModels\Product\ProductVideoResponse;
 use BigCommerce\ApiV3\ResponseModels\Product\ProductVideosResponse;
-use BigCommerce\ApiV3\ResponseModels\SingleResourceResponse;
 
 class ProductVideosApi extends ResourceApi
 {
     public const RESOURCE_NAME   = 'videos';
-    public const VIDEO_ENDPOINT  = 'catalog/products/%s/videos';
-    public const VIDEOS_ENDPOINT = 'catalog/products/%s/videos/%s';
+    public const VIDEO_ENDPOINT  = 'catalog/products/%d/videos/%s';
+    public const VIDEOS_ENDPOINT = 'catalog/products/%d/videos';
 
     protected function singleResourceEndpoint(): string
     {
--- tests/BigCommerce/Catalog/Products/ProductVideosApiTest.php
+++ tests/BigCommerce/Catalog/Products/ProductVideosApiTest.php
@@ -10,16 +10,28 @@ class ProductVideosApiTest extends BigCommerceApiTest
 {
     public function testHasProductAndVideoId(): void
     {
-        $this->markTestIncomplete();
+        $expectedProductId = 687;
+        $expectedVideoId = 3;
+
+        $variantApi = $this->getApi()->catalog()->product($expectedProductId)->video($expectedVideoId);
+        $this->assertEquals($expectedProductId, $variantApi->getParentResourceId());
+        $this->assertEquals($expectedVideoId, $variantApi->getResourceId());
     }
 
     public function testCanGetVideo(): void
     {
-        $this->markTestIncomplete();
+        $this->setReturnData('catalog__products__1__videos__1__get.json');
+
+        $response = $this->getApi()->catalog()->product(1)->video(1)->get();
+        $this->assertEquals('Your Video', $response->getVideo()->title);
     }
 
     public function testCanGetAllVideos(): void
     {
-        $this->markTestIncomplete();
+        $this->setReturnData('catalog__products__192__videos__get_all.json');
+
+        $response = $this->getApi()->catalog()->product(192)->videos()->getAll();
+        $this->assertEquals(3, $response->getPagination()->total);
+        $this->assertEquals('PqBTp23RLhI', $response->getVideos()[0]->video_id);
     }
 }
