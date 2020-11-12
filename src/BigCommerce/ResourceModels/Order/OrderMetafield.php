06d4e8be8deb9624757f9b201779f8aafc03d8d4
Jarrod Swift <jarrod.swift@aligent.com.au>
2020-11-12 14:52:33 +1030

    Implement new Order Metafields api

---
+++ src/BigCommerce/ResourceModels/Order/OrderMetafield.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BigCommerce\ApiV3\ResourceModels\Order;
+
+use BigCommerce\ApiV3\ResourceModels\Metafield;
+
+class OrderMetafield extends Metafield
+{
+    public string $resource_type = 'order';
+}
---
+++ tests/BigCommerce/Orders/OrderMetafieldsApiTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BigCommerce\Tests\Orders;
+
+use BigCommerce\Tests\BigCommerceApiTest;
+
+class OrderMetafieldsApiTest extends BigCommerceApiTest
+{
+    public function testCanGetOrderMetafield(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanGetAllOrderMetafields(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
