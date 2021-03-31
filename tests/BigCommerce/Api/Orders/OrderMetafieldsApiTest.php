f9592798e8f78473f370c5a4355c83532be325d1
Jarrod Swift <jarrod.swift@aligent.com.au>
2021-03-31 14:18:15 +1030

    Implement tests for OrderMetafieldsApi and fix issue with endpoints

--- tests/BigCommerce/Api/Orders/OrderMetafieldsApiTest.php
+++ tests/BigCommerce/Api/Orders/OrderMetafieldsApiTest.php
@@ -8,11 +8,18 @@ class OrderMetafieldsApiTest extends BigCommerceApiTest
 {
     public function testCanGetOrderMetafield(): void
     {
-        $this->markTestIncomplete();
+        $this->setReturnData('orders__2__metafields__3__get.json');
+
+        $this->getApi()->order(2)->metafield(3)->get();
+        $this->assertEquals('orders/2/metafields/3', $this->getLastRequest()->getUri()->getPath());
     }
 
     public function testCanGetAllOrderMetafields(): void
     {
-        $this->markTestIncomplete();
+        $this->setReturnData('orders__1__metafields__get_all.json');
+
+        $response = $this->getApi()->order(1)->metafields()->getAll();
+        $this->assertEquals(2, $response->getPagination()->total);
+        $this->assertEquals('orders/1/metafields', $this->getLastRequest()->getUri()->getPath());
     }
 }
