f5c29fe7d8897fb690676872a0947fe352c301d9
Jarrod Swift <jarrod.swift@aligent.com.au>
2021-04-10 11:33:24 +0930

    Implement the Chanel Site api

---
+++ src/BigCommerce/Api/Channels/ChannelSitesApi.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace BigCommerce\ApiV3\Api\Channels;
+
+use BigCommerce\ApiV3\Api\Generic\CreateResource;
+use BigCommerce\ApiV3\Api\Generic\DeleteResource;
+use BigCommerce\ApiV3\Api\Generic\GetResource;
+use BigCommerce\ApiV3\Api\Generic\UpdateResource;
+use BigCommerce\ApiV3\Api\Generic\V3ApiBase;
+use BigCommerce\ApiV3\ResourceModels\Channel\ChannelSite;
+use BigCommerce\ApiV3\ResponseModels\Channel\ChannelSiteResponse;
+
+class ChannelSitesApi extends V3ApiBase
+{
+    use GetResource;
+    use DeleteResource;
+    use CreateResource;
+    use UpdateResource;
+
+    private const CHANNEL_SITE_ENDPOINT  = 'channels/%d/site';
+
+    public function get(): ChannelSiteResponse
+    {
+        return new ChannelSiteResponse($this->getResource());
+    }
+
+    public function create(ChannelSite $site): ChannelSiteResponse
+    {
+        return new ChannelSiteResponse($this->createResource($site));
+    }
+
+    public function update(ChannelSite $site): ChannelSiteResponse
+    {
+        return new ChannelSiteResponse($this->updateResource($site));
+    }
+
+    public function multipleResourceUrl(): string
+    {
+        return $this->singleResourceUrl();
+    }
+
+    public function singleResourceUrl(): string
+    {
+        return sprintf(self::CHANNEL_SITE_ENDPOINT, $this->getParentResourceId());
+    }
+}
---
+++ src/BigCommerce/ResponseModels/Channel/ChannelSiteResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace BigCommerce\ApiV3\ResponseModels\Channel;
+
+
+use BigCommerce\ApiV3\ResourceModels\Channel\ChannelSite;
+use BigCommerce\ApiV3\ResponseModels\SingleResourceResponse;
+use stdClass;
+
+class ChannelSiteResponse extends SingleResourceResponse
+{
+    private ChannelSite $site;
+
+    public function getSite(): ChannelSite
+    {
+        return $this->site;
+    }
+
+    protected function addData(stdClass $rawData): void
+    {
+        $this->site = new ChannelSite($rawData);
+    }
+}
