8db41726970ab9ba3ba85fcc5b8b62cc0a8014bd
Jarrod Swift <jarrod.swift@aligent.com.au>
2021-07-01 09:42:56 +0930

    Add convenience methods for creating redirects to products and categories

--- src/BigCommerce/ResourceModels/Redirect/Redirect.php
+++ src/BigCommerce/ResourceModels/Redirect/Redirect.php
@@ -5,6 +5,25 @@ namespace BigCommerce\ApiV3\ResourceModels\Redirect;
 use BigCommerce\ApiV3\ResourceModels\ResourceModel;
 use stdClass;
 
+/**
+ * Redirect Resource
+ *
+ * Represents the request object for redirect objects.
+ *
+ * Contains a convenience method for simply redirecting to a product or category
+ *
+ * ```php
+ * $api = new BigCommerce\ApiV3\Client($_ENV['hash'], $_ENV['CLIENT_ID'], $_ENV['ACCESS_TOKEN']);
+ *
+ * $productRedirect = new Redirect();
+ * $productRedirect->site_id = 1000;
+ * $productRedirect->from_path = '/cool-product.html';
+ * $productRedirect->toProduct(123);
+ *
+ * $api->redirects()->upsert([$productRedirect]);
+ * ```
+ *
+ */
 class Redirect extends ResourceModel
 {
     public string $from_path;
@@ -19,5 +38,15 @@ class Redirect extends ResourceModel
         }
 
         parent::__construct($optionObject);
+    }
+
+    public function toProduct(int $productId): void
+    {
+        $this->to = RedirectTo::Build(RedirectTo::TYPE__PRODUCT, $productId);
+    }
+
+    public function toCategory(int $categoryId): void
+    {
+        $this->to = RedirectTo::Build(RedirectTo::TYPE__CATEGORY, $categoryId);
     }
 }
