14ae89b76c47e268c079e50ea4d46255f2e14b2b
Jarrod Swift <jarrod.swift@aligent.com.au>
2021-07-05 11:32:07 +0930

    Usage example for Orders v2

--- src/BigCommerceLegacyApi/Api/Orders/OrdersApi.php
+++ src/BigCommerceLegacyApi/Api/Orders/OrdersApi.php
@@ -6,6 +6,24 @@ use BigCommerce\ApiV2\Api\Generic\V2ApiBase;
 use BigCommerce\ApiV2\ResourceModels\Order\Order;
 use BigCommerce\ApiV3\Api\Generic\CreateResource;
 
+/**
+ * Orders v2 API
+ *
+ * Currently only implements _create_.
+ *
+ * ### Example
+ * $api = new BigCommerce\ApiV2\V2ApiClient($_ENV['hash'], $_ENV['CLIENT_ID'], $_ENV['ACCESS_TOKEN']);
+ *
+ * $order = new BigCommerce\ApiV2\ResourceModels\Order\Order();
+ * // set order details...
+ *
+ * try {
+ *     $createdOrder = $api->orders->create($order);
+ *     echo "Order {$createdOrder->id} has been created.";
+ * } catch (\Psr\Http\Client\ClientExceptionInterface $exception) {
+ *     echo "Unable to update product: {$exception->getMessage()}";
+ * }
+ */
 class OrdersApi extends V2ApiBase
 {
     use CreateResource;
