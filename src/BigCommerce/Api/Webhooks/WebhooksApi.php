976196ebf8dcd551ff55d7225f747fbcc4260935
Jarrod Swift <jarrod.swift@aligent.com.au>
2023-03-29 15:25:35 +1030

    Add Create and Update a Webhook endpoints #187

--- src/BigCommerce/Api/Webhooks/WebhooksApi.php
+++ src/BigCommerce/Api/Webhooks/WebhooksApi.php
@@ -3,6 +3,7 @@
 namespace BigCommerce\ApiV3\Api\Webhooks;
 
 use BigCommerce\ApiV3\Api\Generic\ResourceApi;
+use BigCommerce\ApiV3\ResourceModels\Webhook\Webhook;
 use BigCommerce\ApiV3\ResponseModels\Webhook\WebhookResponse;
 use BigCommerce\ApiV3\ResponseModels\Webhook\WebhooksResponse;
 
@@ -15,6 +16,20 @@ class WebhooksApi extends ResourceApi
     public function get(): WebhookResponse
     {
         return new WebhookResponse($this->getResource());
+    }
+
+    /**
+     * Creates a webhook. Only one webhook at a time can be created. Custom headers can be added.
+     * Destination URL must be served on port 443 (custom ports are not currently supported).
+     */
+    public function create(Webhook $webhook): WebhookResponse
+    {
+        return new WebhookResponse($this->createResource($webhook));
+    }
+
+    public function update(Webhook  $webhook): WebhookResponse
+    {
+        return new WebhookResponse($this->updateResource($webhook));
     }
 
     protected function singleResourceEndpoint(): string
