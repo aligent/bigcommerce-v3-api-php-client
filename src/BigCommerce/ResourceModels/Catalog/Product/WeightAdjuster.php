8f228dac238d16446a707831d1b91bc4575059b0
Qadeer Ahmed <qadeersahmed@gmail.com>
2023-05-07 17:25:34 +0500

    Update WeightAdjuster
    Fix for Null adjuster value from API for FIXED type.

--- src/BigCommerce/ResourceModels/Catalog/Product/WeightAdjuster.php
+++ src/BigCommerce/ResourceModels/Catalog/Product/WeightAdjuster.php
@@ -6,6 +6,18 @@ use BigCommerce\ApiV3\ResourceModels\ResourceModel;
 
 class WeightAdjuster extends ResourceModel
 {
+    public const ADJUSTER_RELATIVE = 'relative';
+    public const ADJUSTER_FIXED    = 'fixed';
+
     public string $adjuster;
     public float $adjuster_value;
+    
+    protected function beforeBuildObject(): void
+    {
+        if (!is_null($this->optionObject)) {
+            if(is_null($this->optionObject->adjuster)){
+                $this->optionObject->adjuster = ADJUSTER_FIXED;
+            }
+        }
+    }
 }
